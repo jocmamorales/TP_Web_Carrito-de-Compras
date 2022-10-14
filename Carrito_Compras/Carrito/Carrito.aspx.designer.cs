49d41678ceece47d9880b2a86f21cfbf75a351d9
RoberRang <rob_rang@hotmail.com>
2022-10-14 15:43:03 -0300

    Importe Total de los productos del carrito

--- Carrito_Compras/Carrito/Carrito.aspx.designer.cs
+++ Carrito_Compras/Carrito/Carrito.aspx.designer.cs
@@ -30,6 +30,15 @@ namespace Carrito
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblTotal;
+        protected global::System.Web.UI.WebControls.Label lblTotal;
+
+        /// <summary>
+        /// Control lblImporte.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblImporte;
     }
 }
