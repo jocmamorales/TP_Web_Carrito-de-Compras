d5ce842a2f9e5a7a4c49e1bf70b93c523ad47694
RoberRang <rob_rang@hotmail.com>
2022-10-14 18:33:14 -0300

    Modelo carrito DTO

---
+++ Carrito_Compras/Carrito/ModelsDtos/ModeloCarritoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Carrito.ModelsDtos
+{
+    public class ModeloCarritoDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal? Precio { get; set; }
+    }
+}
